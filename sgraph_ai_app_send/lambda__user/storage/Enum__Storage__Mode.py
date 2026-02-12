d11b57ded03f93d81e9c544392b08036b3e7e434
Dinis Cruz <dinis.cruz@owasp.org>
2026-02-12 18:05:17 +0000

    Added review of Send_Config changes

--- sgraph_ai_app_send/lambda__user/storage/Enum__Storage__Mode.py
+++ sgraph_ai_app_send/lambda__user/storage/Enum__Storage__Mode.py
@@ -10,5 +10,5 @@ class Enum__Storage__Mode(str, Enum):                                           
     MEMORY = "memory"                                                           # In-memory (default, for dev/test)
     S3     = "s3"                                                               # AWS S3 (production)
 
-    def __str__(self):
+    def __str__(self):              # todo: this should not be needed since in most cases we should be using the Enum value (which Type Safe has good support for)
         return self.value
--- sgraph_ai_app_send/lambda__user/storage/Send__Config.py
+++ sgraph_ai_app_send/lambda__user/storage/Send__Config.py
@@ -12,13 +12,19 @@ from sgraph_ai_app_send.lambda__user.storage.Storage_FS__S3                     
 
 ENV_VAR__SEND__STORAGE_MODE  = 'SEND__STORAGE_MODE'                             # Explicit mode override
 ENV_VAR__SEND__S3_BUCKET     = 'SEND__S3_BUCKET'                               # S3 bucket name
+# todo: this bucket needs to contain both the account-id and the current region
+#       because if we don't we will have issues when deploying this solution across multiple regions
+#       follow the same naming convention used in the S3 bucked used to store lambda data: 745506449035--osbot-lambdas--eu-west-2
+#       you can use "from osbot_aws.AWS_Config import AWS_Config" to get the current account-id and region-name
 DEFAULT__SEND__S3_BUCKET     = 'sgraph-send-transfers'                          # Default bucket
 
-
+# todo: s3_bucket should not be an str (it should be type safe primitive)
 class Send__Config(Type_Safe):                                                  # Storage configuration for Send
     storage_mode : Enum__Storage__Mode = None                                   # Active storage mode
     s3_bucket    : str                 = None                                   # S3 bucket (for S3 mode)
 
+    # todo: add an issue to have a conversation about this, since we really shouldn't be doing any state actions in __init__
+    #       there are multiple ways to achieved this, including the powerful Service Registry that osbot supports
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         if self.storage_mode is None:
@@ -26,10 +32,10 @@ class Send__Config(Type_Safe):                                                  
         self.configure_for_storage_mode()
 
     def determine_storage_mode(self) -> Enum__Storage__Mode:                    # Auto-detect best storage mode
-        explicit = get_env(ENV_VAR__SEND__STORAGE_MODE)
+        explicit = get_env(ENV_VAR__SEND__STORAGE_MODE)                         # todo: add to the conversation mentioned above the fact that we shouldn't be reading en vars in location like this (i.e. this should be in a separate class)
         if explicit:
             try:
-                return Enum__Storage__Mode(explicit.lower())
+                return Enum__Storage__Mode(explicit.lower())                    # todo: this kind of try pattern should never happen since Type_Safe already handles most of these cases
             except ValueError:
                 pass
         if self.has_aws_credentials():
@@ -37,7 +43,7 @@ class Send__Config(Type_Safe):                                                  
         return Enum__Storage__Mode.MEMORY
 
     def has_aws_credentials(self) -> bool:                                      # Check if AWS credentials available
-        if get_env('AWS_ACCESS_KEY_ID') and get_env('AWS_SECRET_ACCESS_KEY'):
+        if get_env('AWS_ACCESS_KEY_ID') and get_env('AWS_SECRET_ACCESS_KEY'):   # todo: check with AWS_Config, since I'm pretty sure it already has this method
             return True
         if get_env('AWS_PROFILE'):
             return True
