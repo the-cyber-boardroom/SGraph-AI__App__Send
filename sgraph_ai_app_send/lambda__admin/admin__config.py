0d4aec6d701754ae517dc3ff2e81ea9039f6d3ca
Dinis Cruz <dinis.cruz@owasp.org>
2026-02-14 00:23:32 +0000

    added lambda dependencies

--- sgraph_ai_app_send/lambda__admin/admin__config.py
+++ sgraph_ai_app_send/lambda__admin/admin__config.py
@@ -3,9 +3,11 @@ from sgraph_ai_app_send import package_name
 APP__SEND__ADMIN__SERVICE_NAME              = package_name
 APP__SEND__ADMIN__FAST_API__TITLE           = "SGraph Send - Admin"
 APP__SEND__ADMIN__FAST_API__DESCRIPTION     = "SGraph Send - Admin"
-APP__SEND__ADMIN__LAMBDA_DEPENDENCIES       = ['httpx==0.28.1'                          ,
-                                               'memory-fs==v0.40.0'                     ,
-                                               'osbot-fast-api-serverless==v1.33.0'     ]
+APP__SEND__ADMIN__LAMBDA_DEPENDENCIES       = ['httpx==0.28.1'                           ,
+                                               'memory-fs==v0.40.0'                      ,
+                                               'mgraph-ai-service-cache==v0.14.0'        ,
+                                               'mgraph-ai-service-cache-client==v0.33.0' ,
+                                               'osbot-fast-api-serverless==v1.33.0'      ]
 
 
 APP_SEND__UI__ADMIN__ROUTE__PATH__CONSOLE    = 'admin'
